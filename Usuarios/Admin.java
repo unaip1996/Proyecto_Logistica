fd5c71a31567ab3bab1e7a31d6531f145366ac97
erkiri <cnavarrodiaz1@alumnos.viu.es>
2023-11-20 11:05:52 +0000

    Primero avances de la clase usuario/ Lógica manejo de usuarios

---
+++ Usuarios/Admin.java
@@ -0,0 +1,28 @@
+package ProyectoLogistica;
+
+public class Admin extends Usuario {
+    
+
+    // Constructor que toma nombre y contraseña como parámetros
+    public Admin(String nick, String password, String numberphone, String mail) {
+        super(nick, password, ADMINTYPE, numberphone, mail);
+        
+    }
+
+    
+    public String getNick(){
+        
+    }
+
+    
+    public String getUserType() {
+        return "Usuario Administrador";
+    }
+
+    
+    public void showhistorial() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Contraseña: " + contraseña);  // Obiamente no mostrar la contraseña, esto es solo un ejemplo
+        System.out.println("Tipo de Usuario: " + getTipoUsuario());
+    }
+}
---
+++ Usuarios/Usuariosprom.java
@@ -0,0 +1,28 @@
+package ProyectoLogistica;
+public class Usuariosprom implements Usuarios {
+    private String nombre;
+    private String contraseña;
+
+    // Constructor que toma nombre y contraseña como parámetros
+    public Usuariosprom(String nombre, String contraseña) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Usuario Promedio";
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Contraseña: " + contraseña);  // No es recomendado mostrar la contraseña, esto es solo un ejemplo
+        System.out.println("Tipo de Usuario: " + getTipoUsuario());
+    }
+}

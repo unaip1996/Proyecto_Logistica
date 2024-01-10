16fab1332af70de29ef6c33d412199bf6ec62e87
Unai Pérez <updev96@gmail.com>
2024-01-10 12:05:26 +0100

    Fix import BuscarController

--- src/main/java/app/ViewControllers/Cliente/BuscarController.java
+++ src/main/java/app/ViewControllers/Cliente/BuscarController.java
@@ -1,23 +1,13 @@
 package app.ViewControllers.Cliente;
 
-import app.DBUtils;
-import app.EntityManager;
+import Util.EntityManager;
 import app.ViewControllers.ViewController;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
